b9dc9ecb1ed91cce175ea93495453b6113c15fdc
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-09-29 21:57:01 -0500

    updated Home component to show items, uncommented line to seed items data

--- client/src/components/Main/Home/Home.js
+++ client/src/components/Main/Home/Home.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_ALL_ITEMS } from "../../../utils/queries"; // Import the GraphQL query
 
 function Home() {
+  // Execute the GraphQL query to fetch item data
+  const { loading, error, data } = useQuery(QUERY_ALL_ITEMS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const items = data.items;
   return (
-    <div>
-      <h2>Welcome to Bundle Spares!</h2>
+    <div className="home">
+      <h2>Home</h2>
+      <div className="item-list">
+        {items.map((items) => (
+          <div key={items._id} className="item-card">
+            <img src={items.img} alt={items.name} />
+            <h3>{items.name}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
